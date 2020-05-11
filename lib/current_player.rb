8847fd1de6810b41cbb76047588dc0a06b32c63a
Stephen C Galvan <stephennoahgalvan@gmail.com>
2020-05-11 02:36:06 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,9 +2,9 @@ board = [" "," "," "," "," "," "," "," "," "]
 def turn_count(board)
   count = 0
   board.each do |space|
-  if space == "X" || "O"
+  if spaces == "X" || spaces == "O"
       count += 1
-  else count = 0
+    end
   end
-  puts "turn #{count}"
+  count 
 end
