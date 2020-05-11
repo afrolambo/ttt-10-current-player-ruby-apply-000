89894e9e1942606d63196e51cc86982646821cd2
Stephen C Galvan <stephennoahgalvan@gmail.com>
2020-05-11 02:49:50 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -10,12 +10,12 @@ def turn_count(board)
 end
 
 def current_player(board)
-  if turn_count.even? || 
+  if turn_count.even? ||
     player == "X"
   elsif turn_count == 0
     player == "X"
   else turn_count.odd?
     player == "O"
   end
-  player 
+  player
 end
